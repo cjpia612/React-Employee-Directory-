6ee0a1eab0e46ce80ca4a6b6eab5d6e954667d49
Cattleya Pia <cjpia612@gmail.com>
2020-08-18 23:54:49 -0700

    added a functional Navbar component, edited API call, downloaded necessary dependencies, & edited App to display navbar component

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
 export default {
-    getRandomEmployees: function () {
-        return axios.get("https://randomuser.me/api/?results=50");
+    getRandomEmployees: function() {
+        return axios
+            .get("https://randomuser.me/api/?results=50")
+            .then(res => {
+                const employees = res.results;
+                console.log(employees);
+                return employees.map(employee => {
+                    return {
+                        picture: employee.picture.medium,
+                        gender: employee.gender,
+                        name: employee.name, 
+                        email: employee.email
+
+                    };
+                }); 
+            });
     }
 };
